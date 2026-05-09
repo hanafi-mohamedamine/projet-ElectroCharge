95c331209afb4bae84f00e62199a857ea5813578
Mohamed amine hanafi <amin1937wac@gmail.com>
2026-05-09 14:28:57 +0100

    Delete projet directory

--- projet/borne/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('borne/', views.liste_bornes),
-    path('sessions/', views.sessions),
-    path('connecteur/', views.connecteur),
-]
